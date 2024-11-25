497054c8bdff7b7ef4a1fd619fa3821f114a9911
Dima.Zhigarev <dimazigarev3898@gmail.com>
2024-11-25 12:56:17 +0400

    Добавил функции, теперь буду разбирать как работает код

--- todolist/db/taskRepository.php
+++ todolist/db/taskRepository.php
@@ -1,5 +1,6 @@
 <?php
 require_once './connection.php';
+
 /**
  * Отвечает за операции сущностью task.
  * ctrl+alt+l выравнивание кода на странице.
@@ -22,6 +23,40 @@ class TaskRepository
         }
         $db->close();
         return $tasks;
+    }
+
+    public function addTask($task)
+    {
+        $db = connectDB();
+        $stmt = $db->prepare('INSERT INTO tasks (title, creationDate, deadline, status) VALUES (:title, :creationDate, :deadline, :status)');
+        $stmt->bindValue(':title', $task['title'], SQLITE3_TEXT);
+        $stmt->bindValue(':creationDate', $task['creationDate'], SQLITE3_TEXT);
+        $stmt->bindValue(':deadline', $task['deadline'], SQLITE3_TEXT);
+        $stmt->bindValue(':status', $task['status'], SQLITE3_TEXT);
+        $stmt->execute();
+        $db->close();
+    }
+
+    public function updateTask($task)
+    {
+        $db = connectDB();
+        $stmt = $db->prepare('UPDATE tasks SET title = :title, creationDate = :creationDate, deadline = :deadline, status = :status WHERE id = :id');
+        $stmt->bindValue(':title', $task['title'], SQLITE3_TEXT);
+        $stmt->bindValue(':creationDate', $task['creationDate'], SQLITE3_TEXT);
+        $stmt->bindValue(':deadline', $task['deadline'], SQLITE3_TEXT);
+        $stmt->bindValue(':status', $task['status'], SQLITE3_TEXT);
+        $stmt->bindValue(':id', $task['id'], SQLITE3_INTEGER);
+        $stmt->execute();
+        $db->close();
+    }
+
+    public function deleteTask($taskId)
+    {
+        $db = connectDB();
+        $stmt = $db->prepare('DELETE FROM tasks WHERE id = :id');
+        $stmt->bindValue(':id', $taskId, SQLITE3_INTEGER);
+        $stmt->execute();
+        $db->close();
     }
 
 }
